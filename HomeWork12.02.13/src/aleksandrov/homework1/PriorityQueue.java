ce89993548b5c2d7771731cf7935f81b2c662075
bourd0n <andone_90@inbox.ru>
2013-02-11 01:48:56 +0400

    Add method realization

--- HomeWork12.02.13/src/aleksandrov/homework1/PriorityQueue.java
+++ HomeWork12.02.13/src/aleksandrov/homework1/PriorityQueue.java
@@ -6,7 +6,7 @@ public class PriorityQueue {
         private Object object;
         private int priority;
 
-        private final int DEFAULT_PRIORITY = 0;
+        private final int DEFAULT_PRIORITY = 1;
 
         QueueElement(Object object, int priority) {
             this.object = object;
@@ -18,30 +18,51 @@ public class PriorityQueue {
             this.priority = DEFAULT_PRIORITY;
         }
 
+        public Object getObject() {
+            return object;
+        }
 
+        public int getPriority() {
+            return priority;
+        }
     }
+
     private QueueElement[] elements;
 
     private int size;
 
+    public final static int DEFAULT_SIZE = 10;
+
     public PriorityQueue() {
-        this.elements = new QueueElement[0];
-        this.size = 0;
+        this.elements = new QueueElement[DEFAULT_SIZE];
+        this.size = DEFAULT_SIZE;
     }
 
     public PriorityQueue(int size){
         this.elements = new QueueElement[size];
-        this.size = 0;
+        this.size = size;
     }
 
     public void add(Object object, int priority){
         if (size >= elements.length){
+            //copy arrays
+            QueueElement[] tempArr = new QueueElement[size * 2];
+            //copy
+            System.arraycopy(elements, 0, tempArr, 0, elements.length);
+            elements = tempArr;
 
         }
-        else{
-            elements.
-        }
 
+        size ++;
+        elements[size] = new QueueElement(object, priority);
+
+        int i = size;
+        while (i > 1 && (elements[i].getPriority() > elements[i/2].getPriority())){
+            QueueElement tempElement = new QueueElement(elements[i/2].getObject(), elements[i/2].getPriority());
+            elements[i/2] = elements[i];
+            elements[i] = tempElement;
+            i = i / 2;
+        }
     }
 
     public Object[] getObjectsWithHightestPriority(){
