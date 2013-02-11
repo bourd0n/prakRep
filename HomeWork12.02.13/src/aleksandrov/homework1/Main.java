ee19c4dd8df925a5a62f42aca4e5b84a677a99fe
bourd0n <andone_90@inbox.ru>
2013-02-12 02:40:15 +0400

    Fix issue with same priorities. But solution can be not good)

--- HomeWork12.02.13/src/aleksandrov/homework1/Main.java
+++ HomeWork12.02.13/src/aleksandrov/homework1/Main.java
@@ -11,22 +11,25 @@ public class Main {
         ftc.set();
         //System.out.println(ftc.get());
         PriorityQueue pq = new PriorityQueue(10);
-        /*pq.add(1, 1);
+        pq.add(1, 1);
         pq.add(2, 2);
         pq.add(4, 4);
         pq.add(5, 5);
         pq.add(6, 6);
         pq.add(8, 8);
-        pq.add(9, 9);*/
+        pq.add(9, 9);
         pq.add(10, 10);
-        /*pq.add(11, 11);
+        pq.add(11, 11);
         pq.add(16, 16);
         pq.add(17, 17);
         pq.add(18, 18);
-        pq.add(19, 19);*/
-        pq.add(16, 10);
-        pq.add(1 , 10);
-        pq.add(2, 10);
+        pq.add(19, 19);
+        //
+        pq.add(21, 10);
+        pq.add(22, 10);
+        pq.add(23, 10);
+        pq.add(24 , 10);
+        pq.add(25, 10);
         System.out.println("size " + pq.size());
         System.out.println(pq);
 
--- HomeWork12.02.13/src/aleksandrov/homework1/task2/PriorityQueue.java
+++ HomeWork12.02.13/src/aleksandrov/homework1/task2/PriorityQueue.java
@@ -10,10 +10,12 @@ public class PriorityQueue {
     private class QueueElement{
         private Object object;
         private int priority;
+        private int source_pos;
 
-        QueueElement(Object object, int priority) {
+        QueueElement(Object object, int priority, int source_pos ) {
             this.object = object;
             this.priority = priority;
+            this.source_pos = source_pos;
         }
 
         QueueElement(Object object){
@@ -61,11 +63,11 @@ public class PriorityQueue {
         }
 
         size ++;
-        elements[size-1] = new QueueElement(object, priority);
+        elements[size-1] = new QueueElement(object, priority, size - 1);
 
         int i = size - 1;
         while (i > 0 && (elements[i].getPriority() > elements[i/2].getPriority())){
-            QueueElement tempElement = new QueueElement(elements[i/2].getObject(), elements[i/2].getPriority());
+            QueueElement tempElement = new QueueElement(elements[i/2].getObject(), elements[i/2].getPriority(), elements[i/2].source_pos);
             elements[i/2] = elements[i];
             elements[i] = tempElement;
             i = i / 2;
@@ -78,7 +80,9 @@ public class PriorityQueue {
         Object result = elements[0].getObject();
         //modify queue
         elements[0] = elements[size - 1];
+        int prefPos = size - 1;
         size--;
+        //heapify(0);
         int i = 0;
         int j;
         while (i < size / 2) {
@@ -88,16 +92,24 @@ public class PriorityQueue {
             //
             //j = 2 * i + 1;
             if (2 * i + 1 < size){
-                if (elements[2 * i + 1].getPriority() >= elements[2 * i + 2].getPriority())
+                if (elements[2 * i + 1].getPriority() > elements[2 * i + 2].getPriority())
                     j = 2 * i + 1;
-                else
+                else if (elements[2 * i + 1].getPriority() < elements[2 * i + 2].getPriority())
                     j = 2 * i + 2;
+                else {//if (elements[2 * i + 1].getPriority() == elements[2 * i + 2].getPriority()){
+                    if (elements[2*i + 1].source_pos > elements[2*i + 2].source_pos)
+                        j = 2 * i + 2;
+                    else
+                        j = 2 * i + 1;
+                }
+
             }
             else
                 j = 2 * i + 1;
             //if (elements[i].getPriority() < elements[j].getPriority()) {
-            if (elements[i].getPriority() <= elements[j].getPriority()) {
-                QueueElement tempElement = new QueueElement(elements[i].getObject(), elements[i].getPriority());
+            //if (elements[i].getPriority() <= elements[j].getPriority()) {
+            if ((elements[i].getPriority() < elements[j].getPriority()) || (elements[i].getPriority() == elements[j].getPriority() && elements[i].source_pos > elements[j].source_pos)) {
+                QueueElement tempElement = new QueueElement(elements[i].getObject(), elements[i].getPriority(), elements[i].source_pos);
                 elements[i] = elements[j];
                 elements[j] = tempElement;
                 i = j;
@@ -110,6 +122,25 @@ public class PriorityQueue {
     public int size(){
         return size;
     }
+
+/*
+    private void heapify(int root)
+    {
+        int left = 2 * root + 1;
+        int right = 2 * root + 2;
+        int largest = root;
+        if (left <= size &&  elements[left].getPriority() >= elements[root].getPriority())
+            largest = left;
+        if (right <= size && elements[right].getPriority() > elements[largest].getPriority())
+            largest = right;
+        if (largest != root){
+            QueueElement tempElement = new QueueElement(elements[root].getObject(), elements[root].getPriority());
+            elements[root] = elements[largest];
+            elements[largest] = tempElement;
+            heapify(largest);
+        }
+    }
+*/
 
 
     @Override
